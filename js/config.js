c2759ca3f2824e5ad1d0c2c51c564c8250400b1d
Omar Mujtaba <omar.mujtaba@gmail.com>
2020-09-03 19:14:02 +1000

    updated cognito config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-southeast-2_DYYFGU8p1', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '16nu33n7m1uh4tvhj3g13mlmbg', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-southeast-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
